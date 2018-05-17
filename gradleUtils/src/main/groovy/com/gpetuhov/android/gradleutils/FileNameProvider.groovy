8435b38c0ab3b447eb7647de77e28ef174054af7
Gennady Petukhov <gpetukhov@n-core.ru>
2018-05-17 11:53:06 +0300

    Add gradleUtils module with FileNameProvider class

---
+++ gradleUtils/src/main/groovy/com/gpetuhov/android/gradleutils/FileNameProvider.groovy
@@ -0,0 +1,8 @@
+package com.gpetuhov.android.gradleutils
+
+// Provides file name that is used in build.gradle to name the artifact being built
+class FileNameProvider {
+    String getFileName() {
+        "my_awesome_app.apk"
+    }
+}
